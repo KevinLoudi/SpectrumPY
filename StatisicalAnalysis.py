3f1385f0c430f7c6e8cb78d8fd43649dbb6e0c44
KevinLoudi <zhugengyuloudi@gmail.com>
2016-10-16 14:12:16 +0800

    some make-up (data and skipped code)

--- StatisicalAnalysis.py
+++ StatisicalAnalysis.py
@@ -6,9 +6,13 @@ Propose: statistical analysis of one-day spectrum data by hour
 @author: kevin
 """
 
+import scipy
+import scipy.stats
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from fitter import Fitter
+
 
 #load csv file for channelstate
 res=pd.read_csv('DayChannelState.csv')
@@ -20,6 +24,37 @@ def occbyhour(hcs):
     totsam=hrow*hcol
     cssum=hcs.sum()
     return cssum/totsam
+    
+#find and count spectrum-hole
+def holesearcher(cs):
+    [tlen,flen]=cs.shape
+    #the possible maxiest hole-length
+    mhole=tlen
+    #decalre an array to store hole num
+    hole=np.arange(mhole+1,dtype=np.float)*0
+    lenindex=0
+    for f in np.arange(flen):
+      #if all time state in a freq-col equals '0'
+        if(lenindex!=0):
+           hole[lenindex]=hole[lenindex]+1
+           lenindex=0  
+        for t in np.arange(tlen):
+            if(cs[t,f]==0):
+                lenindex=lenindex+1 
+            else:
+                hole[lenindex]=hole[lenindex]+1
+                lenindex=0 
+    return hole
+    
+#calculate possible unoccpuied state time-length distribution
+def holelendis(hole):
+    tlen=len(hole)
+    totlen=hole.sum()
+    hish=np.arange(tlen,dtype=np.float)*0
+    for t in np.arange(tlen):
+        hish[t]=hole[t]/totlen
+    return hish
+        
 
 #splite data by hour
 [rown,coln]=cs.shape
@@ -41,11 +76,18 @@ for hcs in csbyh:
     print(tmpocc)
     
 #visualize the daily vary
-plt.plot(occarr)
+#plt.plot(occarr)
+
+#fit data emprical distribution by-hour colwise
+hh=holesearcher(cs)
+hish=holelendis(hh)
+plt.plot(hish)
+plt.xlim(0,47)
 
 #test part
 tarr=np.array([[3,4],[5,6]])
 tsum=tarr.sum()
+tan=np.arange(10)
 
 
 

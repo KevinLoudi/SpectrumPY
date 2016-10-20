644d243d701e5475af516f3120db20646cb0b4d9
KevinLoudi <zhugengyuloudi@gmail.com>
2016-10-20 16:37:47 +0800

    [Kevin]initial a example for general time series analysis

---
+++ SpectrumPrediction_v1.0.py
@@ -0,0 +1,163 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 20 09:59:22 2016
+Test Code Orgin from https://www.analyticsvidhya.com/blog/2016/02/time-series-forecasting-codes-python/
+Propose: Spectrum Prediction via Neural Network
+Step1: Check the Stationarity of data in trends and seasonrily
+Environment: Python 2.7
+@author: kevin
+"""
+import pandas as pd
+import numpy as np
+import matplotlib.pylab as plt
+from matplotlib.pylab import rcParams
+
+rcParams['figure.figsize'] = 15, 6
+
+dateparse = lambda dates: pd.datetime.strptime(dates, '%Y-%m')
+#Python 2.7 because Python can’t identify ‘Month’ as a list of Month 
+#column value for parameter parse_dates
+data = pd.read_csv('AirPassengers.csv', parse_dates=['Month'], index_col=['Month'],date_parser=dateparse)
+print data.head()
+
+print data.index
+ts=data
+
+#Import the datetime library and use 'datetime' function:
+from datetime import datetime
+#print ts[datetime(1949,1,1)]
+
+#Check Stationarity of a Time Series
+from statsmodels.tsa.stattools import adfuller
+
+def test_stationarity(timeseries):
+    
+    #Determing rolling statistics
+    rolmean = pd.rolling_mean(timeseries, window=12)
+    rolstd = pd.rolling_std(timeseries, window=12)
+
+    #Plot rolling statistics:
+    orig = plt.plot(timeseries, color='blue',label='Original')
+    mean = plt.plot(rolmean, color='red', label='Rolling Mean')
+    std = plt.plot(rolstd, color='black', label = 'Rolling Std')
+    plt.legend(loc='best')
+    plt.title('Rolling Mean & Standard Deviation')
+    plt.show(block=False)
+    
+    #Perform Dickey-Fuller test:
+    print 'Results of Dickey-Fuller Test:'
+    # without 'unstack',one may passing in a dataframe instead of a series
+    dftest = adfuller(timeseries.unstack(), autolag='AIC')
+    dfoutput = pd.Series(dftest[0:4], index=['Test Statistic','p-value','#Lags Used','Number of Observations Used'])
+    for key,value in dftest[4].items():
+        dfoutput['Critical Value (%s)'%key] = value
+    print dfoutput
+    
+test_stationarity(ts)
+
+ts_log = np.log(ts)
+plt.plot(ts_log)
+
+#take average of ‘k’ consecutive values to eliminating Trend
+moving_avg = pd.rolling_mean(ts_log,12)
+plt.plot(ts_log)
+plt.plot(moving_avg, color='red')
+
+ts_log_moving_avg_diff = ts_log - moving_avg
+ts_log_moving_avg_diff.head(12)
+
+#eliminate with difference
+ts_log_moving_avg_diff.dropna(inplace=True)
+test_stationarity(ts_log_moving_avg_diff)
+
+#exponentially weighted moving average where weights are 
+#assigned to all the previous values with a decay factor
+expwighted_avg = pd.ewma(ts_log, halflife=12)
+plt.plot(ts_log)
+plt.plot(expwighted_avg, color='red')
+
+#demostrate the results
+ts_log_ewma_diff = ts_log - expwighted_avg
+test_stationarity(ts_log_ewma_diff)
+
+#eliminate Seasonality
+ts_log_diff = ts_log - ts_log.shift()
+plt.plot(ts_log_diff)
+
+ts_log_diff.dropna(inplace=True)
+test_stationarity(ts_log_diff)
+
+#decompse seasonality are modeled trend
+from statsmodels.tsa.seasonal import seasonal_decompose
+decomposition = seasonal_decompose(ts_log)
+
+trend = decomposition.trend
+seasonal = decomposition.seasonal
+residual = decomposition.resid
+
+plt.subplot(411)
+plt.plot(ts_log, label='Original')
+plt.legend(loc='best')
+plt.subplot(412)
+plt.plot(trend, label='Trend')
+plt.legend(loc='best')
+plt.subplot(413)
+plt.plot(seasonal,label='Seasonality')
+plt.legend(loc='best')
+plt.subplot(414)
+plt.plot(residual, label='Residuals')
+plt.legend(loc='best')
+plt.tight_layout()
+
+ts_log_decompose = residual
+ts_log_decompose.dropna(inplace=True)
+test_stationarity(ts_log_decompose)
+
+
+#Forecasting the future series--Auto-Regressive Integrated Moving Averages
+#ACF and PACF plots:
+from statsmodels.tsa.stattools import acf, pacf
+
+lag_acf = acf(ts_log_diff, nlags=20)
+lag_pacf = pacf(ts_log_diff, nlags=20, method='ols')
+
+#Plot ACF: 
+plt.subplot(121) 
+plt.plot(lag_acf)
+plt.axhline(y=0,linestyle='--',color='gray')
+plt.axhline(y=-1.96/np.sqrt(len(ts_log_diff)),linestyle='--',color='gray')
+plt.axhline(y=1.96/np.sqrt(len(ts_log_diff)),linestyle='--',color='gray')
+plt.title('Autocorrelation Function')
+
+#Plot PACF:
+plt.subplot(122)
+plt.plot(lag_pacf)
+plt.axhline(y=0,linestyle='--',color='gray')
+plt.axhline(y=-1.96/np.sqrt(len(ts_log_diff)),linestyle='--',color='gray')
+plt.axhline(y=1.96/np.sqrt(len(ts_log_diff)),linestyle='--',color='gray')
+plt.title('Partial Autocorrelation Function')
+plt.tight_layout()
+
+from statsmodels.tsa.arima_model import ARIMA
+#model = ARIMA(ts_log, order=(2, 1, 0))  
+#results_AR = model.fit(disp=-1)  
+#plt.plot(ts_log_diff)
+#plt.plot(results_AR.fittedvalues, color='red')
+#plt.title('RSS: %.4f'% sum((results_AR.fittedvalues-ts_log_diff)**2))
+
+#model = ARIMA(ts_log, order=(0, 1, 2))  
+#results_MA = model.fit(disp=-1)  
+#plt.plot(ts_log_diff)
+#plt.plot(results_MA.fittedvalues, color='red')
+#plt.title('RSS: %.4f'% sum((results_MA.fittedvalues-ts_log_diff)**2))
+
+#model = ARIMA(ts_log, order=(2, 1, 2))  
+#results_ARIMA = model.fit(disp=-1)  
+#plt.plot(ts_log_diff)
+#plt.plot(results_ARIMA.fittedvalues, color='red')
+#plt.title('RSS: %.4f'% sum((results_ARIMA.fittedvalues-ts_log_diff)**2))
+
+#predictions_ARIMA_diff = pd.Series(results_ARIMA.fittedvalues, copy=True)
+#print predictions_ARIMA_diff.head()
+#predictions_ARIMA_diff_cumsum = predictions_ARIMA_diff.cumsum()
+#print predictions_ARIMA_diff_cumsum.head()
